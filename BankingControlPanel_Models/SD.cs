e97455d759eb2259ba81f8520b47e65203ca9497
safaKhan <safa3.1998@hotmail.com>
2024-08-17 16:26:06 +0300

    Add project files.

---
+++ BankingControlPanel_Models/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingControlPanel_Models
+{
+    public class SD  //static details
+    {
+
+    }
+}
